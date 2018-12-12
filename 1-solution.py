3ea14e8da102bce7ab9a30353a80a76cad737736
Paul Bond <git@paulsbond.co.uk>
2018-12-12 23:48:52 +0000

    Day 1

---
+++ 1-solution.py
@@ -0,0 +1,16 @@
+changes = [int(line) for line in open("1-input")]
+
+# Part 1
+print(sum(changes))
+
+# Part 2
+frequency = 0
+frequencies_reached = {0}
+i = 0
+for _ in range(1000000):
+  frequency += changes[i]
+  if frequency in frequencies_reached: break
+  frequencies_reached.add(frequency)
+  i += 1
+  if i == len(changes): i = 0
+print(frequency)
