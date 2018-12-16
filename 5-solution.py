a97affda3017964fcaf6d4681eec2d8ab22be387
Paul Bond <git@paulsbond.co.uk>
2018-12-16 12:44:21 +0000

    Removed unnecessary function Fixed input file name

--- 5-solution.py
+++ 5-solution.py
@@ -1,7 +1,7 @@
 import string
 import re
 
-polymer = open("5-input").read().strip()
+polymer = open("5-input.txt").read().strip()
 
 def can_react(units):
   return len(units) == 2 and abs(ord(units[0]) - ord(units[1])) == 32
@@ -12,20 +12,13 @@ def react(polymer):
       polymer = polymer[:i] + polymer[i+2:]
   return polymer
 
-def fully_react(polymer):
-  while True:
-    new_polymer = react(polymer)
-    if len(new_polymer) == len(polymer): break
-    polymer = new_polymer
-  return new_polymer
-
 # Part 1
-print(len(fully_react(polymer)))
+print(len(react(polymer)))
 
 # Part 2
 lengths = []
 for unit_type in string.ascii_lowercase:
   remaining = re.sub(unit_type, "", polymer, flags=re.IGNORECASE)
-  length = len(fully_react(remaining))
+  length = len(react(remaining))
   lengths.append(length)
 print(min(lengths))
