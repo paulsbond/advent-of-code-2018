254f6b058a44822fdd4ff37d49b32ade313b0498
Paul Bond <git@paulsbond.co.uk>
2018-12-18 23:16:51 +0000

    Day 9 (Part 2)

--- 9-solution.py
+++ 9-solution.py
@@ -1,33 +1,24 @@
 from itertools import cycle
+from collections import deque
 
 class Player:
   def __init__(self):
     self.score = 0
 
-# TODO: Improve speed
-def winning_points(n_players, n_marbles):
+def winning_points(n_players, max_points):
   players = [Player() for _ in range(n_players)]
-  queue = cycle(players)
+  player_cycle = cycle(players)
 
-  circle = [0]
-  current = 0
-  for marble in range(1, n_marbles + 1):
-    player = next(queue)
-    if marble % 23 != 0:
-      index = current + 2
-      while index > len(circle): index -= len(circle)
-      while index < 0: index += len(circle)
-      circle.insert(index, marble)
-      current = index
+  circle = deque([0])
+  for marble in range(1, max_points + 1):
+    player = next(player_cycle)
+    if marble % 23 == 0:
+      circle.rotate(7)
+      player.score += marble + circle.pop()
+      circle.rotate(-1)
     else:
-      player.score += marble
-      index = current - 7
-      while index > len(circle): index -= len(circle)
-      while index < 0: index += len(circle)
-      player.score += circle.pop(index)
-      while index > len(circle): index -= len(circle)
-      while index < 0: index += len(circle)
-      current = index
+      circle.rotate(-1)
+      circle.append(marble)
 
   return max(player.score for player in players)
 
@@ -35,4 +26,4 @@ def winning_points(n_players, n_marbles):
 print(winning_points(416, 71617))
 
 # Part 2
-# print(winning_points(416, 71617 * 100))
+print(winning_points(416, 71617 * 100))
