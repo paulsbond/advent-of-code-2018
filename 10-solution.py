b947afebf5936e5e095b66f0c31e37428c538abe
Paul Bond <git@paulsbond.co.uk>
2018-12-19 17:31:54 +0000

    Added check on area

--- 10-solution.py
+++ 10-solution.py
@@ -11,22 +11,37 @@ class Point:
     y = self.position[1] + seconds * self.velocity[1]
     return [x, y]
 
-def print_grid(points, seconds):
-  min_x = min(p.position_after(seconds)[0] for p in points)
-  max_x = max(p.position_after(seconds)[0] for p in points)
-  min_y = min(p.position_after(seconds)[1] for p in points)
-  max_y = max(p.position_after(seconds)[1] for p in points)
+def print_grid(positions):
+  min_x = min(position[0] for position in positions)
+  max_x = max(position[0] for position in positions)
+  min_y = min(position[1] for position in positions)
+  max_y = max(position[1] for position in positions)
   grid = [["." for x in range(min_x, max_x + 1)] for y in range(min_y, max_y + 1)]
-  for point in points:
-    x = point.position_after(seconds)[0] - min_x
-    y = point.position_after(seconds)[1] - min_y
+  for position in positions:
+    x = position[0] - min_x
+    y = position[1] - min_y
     grid[y][x] = "X"
   for i in range(len(grid)):
     print("".join(grid[i]))
 
-points = [Point(line) for line in open("10-test.txt")]
+def get_area(positions):
+  min_x = min(position[0] for position in positions)
+  max_x = max(position[0] for position in positions)
+  min_y = min(position[1] for position in positions)
+  max_y = max(position[1] for position in positions)
+  return (max_x - min_x) * (max_y - min_y)
 
-for seconds in range(5):
-  print("#" * 30)
-  print_grid(points, seconds)
-  print("#" * 30)
+points = [Point(line) for line in open("10-test.txt")]
+positions = [point.position for point in points]
+min_area = get_area(positions)
+min_area_seconds = 0
+
+for seconds in range(1, 5):
+  positions = [point.position_after(seconds) for point in points]
+  area = get_area(positions)
+  if area < min_area:
+    min_area = area
+    min_area_seconds = seconds
+
+positions = [point.position_after(min_area_seconds) for point in points]
+print_grid(positions)
